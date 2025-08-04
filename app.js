f71a51b52c14a1e94479e6e1b630f6a0709e7d73
BryanNrgPl <bryan772911@gmail.com>
2025-08-04 16:28:26 -0500

    actualizacion

--- app.js
+++ app.js
@@ -5,5 +5,34 @@ function obtenerInput(){
 }
 
 function vaciarCaja() {
-    
+    document.getElementById('amigo').value = "";
+}
+
+//Boton agregar amigos
+function agregarAmigo() {
+
+    let amigo = obtenerInput();
+    //Si amigo contiene espacios vacios O es un número
+    if(amigo.trim() === "" || !isNaN(amigo)){ 
+        alert("Ingresa un nombre valido");  // muestra alerta
+        vaciarCaja();
+    }else{
+        listaDeAmigos.push(amigo);  // de otra manera agrega a la lista
+        actualizarLista();
+        vaciarCaja();
+        console.log(listaDeAmigos);
+    }
+}
+
+function actualizarLista() {
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = "";
+
+    let amigosLi = "";
+
+    for (let index = 0; index < listaDeAmigos.length; index++) {
+        amigosLi += "<li>" + listaDeAmigos[index] + "</li>";
+    }
+
+    listaHTML.innerHTML = amigosLi;
 }
